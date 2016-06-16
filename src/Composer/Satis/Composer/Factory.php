134d37b2df5376199ea9eb653dfcdf440d429290
Andrew Roslik <andkirby@gmail.com>
2016-06-16 16:51:21 +0300

    Applied new changes in composer for initializing repositories.

--- src/Composer/Satis/Composer/Factory.php
+++ src/Composer/Satis/Composer/Factory.php
@@ -17,23 +17,54 @@ use Composer\Util\RemoteFilesystem;
 class Factory extends \Composer\Factory
 {
     /**
-     * Added additional required VCS types
-     * (e.g.: "vcs-namespace")
+     * Add custom repositories
      *
-     * @param  IOInterface                    $io
-     * @param  Config                         $config
-     * @param  EventDispatcher                $eventDispatcher
-     * @param  RemoteFilesystem               $rfs
-     * @return RepositoryManager
+     * @param RepositoryManager $rm
+     * @return $this
      */
-    protected function createRepositoryManager(IOInterface $io, Config $config, EventDispatcher $eventDispatcher = null, RemoteFilesystem $rfs = null)
+    public function addCustomRepositoryClasses(RepositoryManager $rm)
     {
-        $rm = parent::createRepositoryManager($io, $config, $eventDispatcher);
         if (!isset($rm->getRepositories()['gitlab'])) {
             $rm->setRepositoryClass('gitlab', 'AndKirby\Composer\MultiRepo\Repository\GitLabRepository');
         }
+
         $rm->setRepositoryClass('gitlab-namespace', 'AndKirby\Composer\MultiRepo\Repository\GitLabNamespaceRepository');
         $rm->setRepositoryClass('vcs-namespace', 'AndKirby\Composer\MultiRepo\Repository\VcsNamespaceRepository');
+
+        return $this;
+    }
+
+    /**
+     * Added additional required VCS types
+     * (e.g.: "vcs-namespace")
+     *
+     * @param  IOInterface      $io
+     * @param  Config           $config
+     * @param  EventDispatcher  $eventDispatcher
+     * @param  RemoteFilesystem $rfs
+     * @return RepositoryManager
+     */
+    protected function createRepositoryManager(IOInterface $io, Config $config, EventDispatcher $eventDispatcher = null,
+        RemoteFilesystem $rfs = null
+    ) {
+        $rm = parent::createRepositoryManager($io, $config, $eventDispatcher);
+
+        $this->addCustomRepositoryClasses($rm);
+
         return $rm;
+    }
+
+    /**
+     * Inject adding namespaced/custom repositories
+     *
+     * @param \Composer\IO\IOInterface               $io
+     * @param \Composer\Repository\RepositoryManager $rm
+     * @param string                                 $vendorDir
+     */
+    protected function addLocalRepository(IOInterface $io, RepositoryManager $rm, $vendorDir)
+    {
+        $this->addCustomRepositoryClasses($rm);
+
+        parent::addLocalRepository($io, $rm, $vendorDir);
     }
 }
